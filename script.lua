3fdd52ffec5b7ecc87133de8a2e003c5497102f7
Christian <58045620+Christian2703@users.noreply.github.com>
2024-05-08 06:01:46 -0300

    script.lua

--- script.lua
+++ script.lua
@@ -19,8 +19,28 @@ end
 _G.RankUp = true
 _G.Teleport = true
 _G.AutoAtk = true
-
+_G.Eggs = true
         --funcao
+
+        function Eggs()
+            while _G.Eggs == true do
+                
+                local args = {
+                    [1] = {
+                        [1] = "BuyTier",
+                        [2] = workspace:WaitForChild("Client"):WaitForChild("Summon Stars"):WaitForChild("Demon Slayer"):WaitForChild("Basic Tier"),
+                        [3] = "E",
+                        [4] = "Demon Slayer"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                wait(0.5)
+            end
+        end
+
+
+
         function AutoAtk()
             while _G.AutoAtk == true do
                 
@@ -68,15 +88,6 @@ local Section = autofarmtab:AddSection({
 
 
 autofarmtab:AddToggle({
-    Name = "Rank up",
-    Default = false,
-    Callback = function(Value)
-    _G.RankUp = Value
-    RankUp()
-    end
-})
-
-autofarmtab:AddToggle({
     Name = "AutoAtk",
     Default = false,
     Callback = function(Value)
@@ -85,7 +96,160 @@ autofarmtab:AddToggle({
     end
 })
 
+
+autofarmtab:AddToggle({
+    Name = "Rank up",
+    Default = false,
+    Callback = function(Value)
+    _G.RankUp = Value
+    RankUp()
+    end
+})
+
+
+    --AutoEgg TAB
+    --AutoEgg TAB
+    --AutoEgg TAB
+
+
+local AutoEggTab = Window:MakeTab({
+    Name = "AutoEgg",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+
+local Section = AutoEggTab:AddSection({
+    Name = "Eggs"
+})
+
+
+AutoEggTab:AddToggle({
+    Name = "Demon Slayer",
+    Callback = function(Value)
+        _G.Eggs = Value
+        Eggs()
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Magic Academy",
+    Callback = function()
+
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Dragon Planet",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Shinobi Vilage",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Virtual Town",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Wall Sentinels",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Sea of Dreams",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Clover Village",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Hero City",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Tokyo Warzone",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Sins Kingdom",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Ant Village",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Bizarre Town",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Chain City",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Metropolis City",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Tokyo city",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+AutoEggTab:AddToggle({
+    Name = "Solo City",
+    Callback = function()
+              print("button pressed")
+      end    
+})
+
+
         --Teleport WORLDS
+        --Teleport WORLDS
+        --Teleport WORLDS
+
+
         local TeleportTab = Window:MakeTab({
             Name = "Teleport ",
             Icon = "rbxassetid://4483345998",
@@ -94,37 +258,266 @@ autofarmtab:AddToggle({
 
 
         local Section = TeleportTab:AddSection({
-            Name = "Teleport"
+            Name = "Teleport world"
         })
 
-
         TeleportTab:AddButton({
-            Name = "Lobby",
+            Name = "Trial",
             Callback = function()
-                local player = game.Players.LocalPlayer.Character
-                local part = game.Workspace.Client.Maps["Demon Slayer"].SpawnPoint
-                player.HumanoidRootPart.CFrame = part.CFrame
-                wait(.5)
+                local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+                local function entrarNoTrial()
+                    local args = {
+                        [1] = {
+                            [1] = "ChangeZone",
+                            [2] = "Trial Easy"
+                        }
+                    }
+                    
+                    ReplicatedStorage.Remotes.Server:FireServer(unpack(args))
+                    
+                    wait(1) -- Espera um segundo antes de chamar a próxima função
+                    
+                    local args2 = {
+                        [1] = {
+                            [1] = "EnterTrial",
+                            [2] = "Easy"
+                        }
+                    }
+                    
+                    ReplicatedStorage.Remotes.Server:FireServer(unpack(args2))
+                end
+                
+                -- Chama a função para entrar no trial
+                entrarNoTrial()                
               end    
         })
 
         TeleportTab:AddButton({
-            Name = "trial",
+            Name = "Demon Slayer",
             Callback = function()
                 local args = {
                     [1] = {
                         [1] = "ChangeZone",
-                        [2] = "Trial Lobby"
+                        [2] = "Demon Slayer"
                     }
                 }
                 
                 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
-                wait(1.0)
+                wait(.5)                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Magic Academy",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Jujutsu Kaisen"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Dragon Planet",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Dragon Ball"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Shinobi Vilage",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Naruto"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Virtual Town",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Sword Art Online"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Wall Sentinels",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Attack On Titan"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Sea of Dreams",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "One Piece"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Clover Village",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Black Clover"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Hero City",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "My Hero Academia"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Tokyo Warzone",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Tokyo Revengers"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Sins Kingdom",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "7DS"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Ant Village",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "HxH"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Bizarre Town",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "ChangeZone",
+                        [2] = "Jojo"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Chain City",
+            Callback = function()
+                      print("button pressed")
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Metropolis City",
+            Callback = function()
+                      print("button pressed")
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Tokyo city",
+            Callback = function()
+                      print("button pressed")
+              end    
+        })
+
+        TeleportTab:AddButton({
+            Name = "Solo City",
+            Callback = function()
+                      print("button pressed")
               end    
         })
 
 
-        --Teleport WORLDS
+        --MISC
         local MiscTab = Window:MakeTab({
             Name = "Misc",
             Icon = "rbxassetid://4483345998",
@@ -139,7 +532,7 @@ autofarmtab:AddToggle({
 
 
         MiscTab:AddButton({
-            Name = "MARKS",
+            Name = "Marks/Marcas",
             Callback = function()
                 local args = {
                     [1] = {
@@ -152,21 +545,16 @@ autofarmtab:AddToggle({
               end    
         })
 
+
         MiscTab:AddButton({
-            Name = "EnterTrial",
+            Name = "Curses/Maldições",
             Callback = function()
-                local args = {
-                    [1] = {
-                        [1] = "CheckInTrial"
-                    }
-                }
-                
-                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
-                wait(1.0)
-              end    
+            local player = game.Players.LocalPlayer.Character
+            local part = game.Workspace.Client.Maps["7DS"].Debris["Area 2x"]
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
         })
-
-
 
         MiscTab:AddButton({
             Name = "shiny",
@@ -177,6 +565,74 @@ autofarmtab:AddToggle({
             wait(.5)
         end    
         })
+
+        MiscTab:AddButton({
+            Name = "Auras/",
+            Callback = function()
+            local player = game.Players.LocalPlayer.Character
+            local part = game.workspace.Client.Maps["Dragon Ball"].Interact.Shiny
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
+        })
+
+        MiscTab:AddButton({
+            Name = "Sword Enchant",
+            Callback = function()
+            local player = game.Players.LocalPlayer.Character
+            local part = game.workspace.Client.Maps["Dragon Ball"].Interact.Shiny
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
+        })
+
+        MiscTab:AddButton({
+            Name = "Grimoires",
+            Callback = function()
+                local args = {
+                    [1] = {
+                        [1] = "SpinGrimoire",
+                        [2] = "Grimoire1"
+                    }
+                }
+                
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+                wait(.5)
+        end    
+        })
+
+        MiscTab:AddButton({
+            Name = "Quirks",
+            Callback = function()
+            local player = game.Players.LocalPlayer.Character
+            local part = game.Workspace.Client.Maps["7DS"].Debris["Area 2x"]
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
+        })
+
+        MiscTab:AddButton({
+            Name = "Area 2x",
+            Callback = function()
+            local player = game.Players.LocalPlayer.Character
+            local part = game.Workspace.Client.Maps["7DS"].Debris["Area 2x"]
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
+        })
+
+        MiscTab:AddButton({
+            Name = "Stands",
+            Callback = function()
+            local player = game.Players.LocalPlayer.Character
+            local part = game.Workspace.Client.Maps["7DS"].Debris["Area 2x"]
+            player.HumanoidRootPart.CFrame = part.CFrame
+            wait(.5)
+        end    
+        })
+
+
+
         --local part = game.Workspace.Client.Maps["Demon Slayer"].SpawnPoint
 
         --local part = game.workspace.Client.Maps["Dragon Ball"].interact.Shiny
@@ -317,3 +773,42 @@ local toggleTeleportLoopButton3 = autofarmtab:AddToggle({
         end
     end
 })
+
+
+
+local toggleTeleportLoopButton1 = autofarmtab:AddToggle({
+    Name = "Test trial",
+    Title = "Ativar Loop de Teleport (M01-M07)",
+    Description = "Ativa ou desativa o loop de teleport para M01-M07",
+    Callback = function(isEnabled)
+        isTeleportLoopEnabled = isEnabled
+        if isTeleportLoopEnabled then
+            -- Iniciar o loop de teleport para M01-M07
+            teleportLoop({
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M01.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M02.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M03.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M04.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M05.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M06.Head,
+                game.Workspace.Client.Maps["Demon Slayer"].Mobs.M07.Head
+            })
+        end
+    end
+})
+
+
+
+
+
+local args = {
+    [1] = {
+        [1] = "BuyTier",
+        [2] = workspace:WaitForChild("Client"):WaitForChild("Summon Stars"):WaitForChild("Demon Slayer"):WaitForChild("Basic Tier"),
+        [3] = "E",
+        [4] = "Demon Slayer"
+    }
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Server"):FireServer(unpack(args))
+wait(.5)   
